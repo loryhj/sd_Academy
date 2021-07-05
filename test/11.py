5dc624d4566e4a0c2b0f18a0558249061157b6c4
sdedu <sdedu@DESKTOP-I9LP8OI>
2021-07-05 17:09:04 +0900

    0705

--- test/11.py
+++ test/11.py
@@ -1,5 +1,36 @@
-'''
-Created on 2021. 7. 5.
+# -*- coding:utf-8 -*-
 
-@author: sdedukllklklk1111
-''' 
+#pip install opencv-python 설치
+
+import cv2
+import matplotlib.pyplot as plt
+
+
+#grayscale : 픽셀단위로 색상 표기해주는 기능 (흰색은 255, 검은색은 0)
+# data = cv2.imread("D:/sunny/sad.png", cv2.IMREAD_GRAYSCALE) 
+# print(data)
+# plt.imshow(data, cmap='gray') # 좌표, 컬러맵
+# plt.show()
+
+#BGR
+data = cv2.imread("D:/sunny/sad.png", cv2.IMREAD_COLOR) #BGR
+print(data)
+
+data = cv2.cvtColor(data, cv2.COLOR_BGR2RGB) #BGR -> RGB로
+plt.imshow(data)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
