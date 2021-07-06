b191a73ba1740a0bb832ca8eeb33cd14dedfa00d
sdedu <sdedu@DESKTOP-3LDIBLV>
2021-07-06 17:51:34 +0900

    bigdata_reference

---
+++ git_project/bigData_reference.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+
+# [코딩 참고 사이트]
+# 생활코딩 : https://opentutorials.org/course/1
+# 자바웹프로그래밍 : https://freelec.co.kr/lecture/%EC%B5%9C%EC%8B%A0-%ED%91%9C%EC%A4%80-c%EB%A1%9C-%EC%89%BD%EA%B3%A0-%EB%B9%A0%EB%A5%B4%EA%B2%8C-%EC%95%88%EB%82%B4%ED%95%98%EB%8A%94/
+# 우아한형제들 기술블로그 : https://techblog.woowahan.com/4159/
+
+
+# [데이터분석 원천자료]
+# 상권분석: https://sg.sbiz.or.kr/godo/index.sg
+
+
+# [단기 빅데이터 교육]
+# 환경 빅데이터 활용 및 인재양성(7월 10일(토) ~ 7월 24일(토))
+# https://docs.google.com/forms/d/e/1FAIpQLScdqEVQ8R5sSCN-KXPhDM42z6yvnS2fToswMnaT4aPigg2-Dg/viewform
+
+# BC카드 금융 빅데이터 플랫폼 데이터 활용 실무 교육(무료) (7/16 금) 
+# https://event-us.kr/ZSFhYS2hdJU8/User/AttendResult/33092?State=Non
+
+# IT 비전공자를 위한 데이터분석 Build-Up 세미나(2021.07.23 금)
+# https://academy.sba.kr/course/course_view.jsp?id=29666&cid=6659&lnb=6681&ch=course
+# https://academy.sba.kr/course/course_view.jsp?id=29666&cid=6659&lnb=6681&ch=course
+print()
--- git_project/tt.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-# 생활코딩 : https://opentutorials.org/course/1
-# https://sg.sbiz.or.kr/godo/index.sg
-print()
